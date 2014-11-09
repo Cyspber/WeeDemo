296dbdb422858c13208cc65db926a25137b2b8c7
Cyspber <Cyspber@gmail.com>
2014-11-09 17:34:00 +0800

    Initial Commit

---
+++ WeeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeeDemo
+//
+//  Created by Wee on 11/9/14.
+//  Copyright (c) 2014 Wallee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WeeDemo
+//
+//  Created by Wee on 11/9/14.
+//  Copyright (c) 2014 Wallee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
